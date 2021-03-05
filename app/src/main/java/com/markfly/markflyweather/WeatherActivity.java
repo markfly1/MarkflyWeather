6de5a51c8cb3d031326c7947e9d64898d1a02f94
Tim <417228973@qq.com>
2021-03-05 23:01:09 +0800

    增加后台自动更新天气的功能(暂时完结)

--- app/src/main/java/com/markfly/markflyweather/WeatherActivity.java
+++ app/src/main/java/com/markfly/markflyweather/WeatherActivity.java
@@ -1,12 +1,12 @@
 package com.markfly.markflyweather;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.markfly.markflyweather.gson.Forecast;
 import com.markfly.markflyweather.gson.Weather;
+import com.markfly.markflyweather.service.AutoUpdateService;
 import com.markfly.markflyweather.util.HttpUtil;
 import com.markfly.markflyweather.util.Utility;
 
@@ -199,6 +200,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather", responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
 
